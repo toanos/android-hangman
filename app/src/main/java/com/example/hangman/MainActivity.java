0574e7bdb1aae1f68f5e45791f2b72eead344b54
toanos <toan.ha1896@gmail.com>
2021-10-24 08:30:51 -0700

    MainActivity.java: instantiate game object.

--- app/src/main/java/com/example/hangman/MainActivity.java
+++ app/src/main/java/com/example/hangman/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.hangman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Hangman game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // If Hangman object is not instantiated (ie. game == null).
+        // Then, construct game with default guesses
+        if (game == null) {
+            game = new Hangman(Hangman.DEFAULT_GUESSES);
+        }
         setContentView(R.layout.activity_main);
+        TextView status = (TextView) findViewById(R.id.status);
+        status.setText("" + game.getGuessesLeft());
+    }
+
+    public void play() {
+        // TODO implement play()
     }
 }
